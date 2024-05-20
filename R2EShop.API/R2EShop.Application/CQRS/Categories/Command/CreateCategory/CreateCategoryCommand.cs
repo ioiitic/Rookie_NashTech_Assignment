4c54976e397e0c0ff81a9b045b58053ba543cc5f
ioiitic <khoitranminh592003@gmail.com>
2024-05-20 10:51:41 +0700

    [R2E02_02] Init create category

---
+++ R2EShop.API/R2EShop.Application/CQRS/Categories/Command/CreateCategory/CreateCategoryCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using R2EShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2EShop.Application.CQRS.Categories.Command.CreateCategory
+{
+    public record CreateCategoryCommand(
+        string CategoryName,
+        string PhotoUrl) : IRequest<Unit>;
+}
---
+++ R2EShop.API/R2EShop.Application/CQRS/Categories/Command/CreateCategory/CreateCategoryCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using R2EShop.Application.Interface.Repositories;
+using R2EShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2EShop.Application.CQRS.Categories.Command.CreateCategory
+{
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, Unit>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CreateCategoryCommandHandler(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<Unit> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            // 1. Create new category
+            Category newCategory = new Category
+            {
+                CategoryName = request.CategoryName,
+                PhotoUrl = request.PhotoUrl,
+            };
+
+            // 2. Create category
+            await _categoryRepository.AddAsync(newCategory);
+
+            return Unit.Value;
+        }
+    }
+}
