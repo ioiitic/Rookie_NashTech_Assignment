c623942fa680ed551a2ddc3c355ce1dcd7b23ab8
ioiitic <khoitranminh592003@gmail.com>
2024-05-20 10:39:45 +0700

    [R2E02_01] Init get categories

---
+++ R2EShop.API/R2EShop.Application/CQRS/Categories/Queries/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using R2EShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2EShop.Application.CQRS.Categories.Queries.GetCategories
+{
+    public record GetCategoriesQuery() : IRequest<IList<Category>>;
+}
---
+++ R2EShop.API/R2EShop.Application/CQRS/Categories/Queries/GetCategories/GetCategoriesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿
+
+using MediatR;
+using R2EShop.Application.Interface.Repositories;
+using R2EShop.Domain.Entities;
+
+namespace R2EShop.Application.CQRS.Categories.Queries.GetCategories
+{
+    public class GetCategoriesQueryHandler : IRequestHandler<GetCategoriesQuery, IList<Category>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public GetCategoriesQueryHandler(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IList<Category>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            // 1. Get list category
+            var categories = await _categoryRepository.GetAllAsync();
+
+            return categories.ToList();
+        }
+    }
+}
