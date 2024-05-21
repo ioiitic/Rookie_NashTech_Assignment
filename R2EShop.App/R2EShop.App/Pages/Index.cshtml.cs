e381690f4c90d81ff2506b361fc479a6b61a1c10
ioiitic <khoitranminh592003@gmail.com>
2024-05-21 13:57:24 +0700

    Init App

---
+++ R2EShop.App/R2EShop.App/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace R2EShop.App.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
