4888ded3b62e60be94d25757c73d1204027f05d8
ioiitic <khoitranminh592003@gmail.com>
2024-05-23 17:13:40 +0700

    Init Navbar

---
+++ R2EShop.API/R2EShop.Contracts/CaseTypeContract/GetCaseTypeResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2EShop.Contracts.CaseTypeContract
+{
+    public record GetCaseTypeResponse(
+        Guid Id,
+        string CaseTypeName,
+        int Protection,
+        int Weight,
+        double AverageStar);
+}
