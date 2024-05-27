c8ee9126fa61baff3501d42e4abce69ece1df5b0
ioiitic <khoitranminh592003@gmail.com>
2024-05-27 09:14:47 +0700

    Create UI HomePag and ProductPage

---
+++ R2EShop.App/R2EShop.App/Pages/PhoneCase/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace R2EShop.App.Pages.PhoneCase
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
